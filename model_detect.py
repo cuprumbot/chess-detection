a6969bb88766ded908ed37217e6fdb6e7661b3cd
Luis Cu <luiscu@galileo.edu>
2025-06-12 18:36:12 -0600

    Add corner detection

--- model_detect.py
+++ model_detect.py
@@ -7,17 +7,29 @@ from IPython.display import Video
 import glob
 import matplotlib.pyplot as plt
 import warnings
+import torch
 
 warnings.filterwarnings('ignore')
+
+def print_used_memory():
+    if torch.cuda.is_available():
+        allocated = torch.cuda.memory_allocated() / 1024**2
+        reserved = torch.cuda.memory_reserved() / 1024**2
+        print(f"GPU Memory Allocated: {allocated:.2f} MB")
+        print(f"GPU Memory Reserved:  {reserved:.2f} MB")
 
 def main():
     ultralytics.checks()
 
-    model = YOLO('models/trained_model_mid.pt')
+    corner_model = YOLO('models/trained_model_corners.pt')
+    pieces_model = YOLO('models/trained_model_pieces.pt')
     
-    example_video_path = r'C:\Users\luisr\chess-detection\dataset\video.mp4'
+    path = r'dataset\roboflow.jpg'
 
-    video_output = model.predict(source=example_video_path, conf=0.5,save=True)
+    corner = corner_model.predict(source=path, conf=0.1, save=True, project='runs', name='corner', exist_ok=True)
+    pieces = pieces_model.predict(source=path, conf=0.5, save=True, project='runs', name='pieces', exist_ok=True)
+
+    print_used_memory()
 
 if __name__ == '__main__':
     from multiprocessing import freeze_support
